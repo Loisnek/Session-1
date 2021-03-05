beab1840eb710c0d290bd2e907f73d25463bf9c8
Loisnek <Loisnek@yandex.ru>
2021-03-05 12:52:07 +0300

    ДМ

---
+++ WpfApp2/WpfApp2/ClientWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientWindow.xaml
+    /// </summary>
+    public partial class ClientWindow : Window
+    {
+        public ClientWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteC(object sender, RoutedEventArgs e)
+        {
+            var db = new dbContext();
+            db.clients.Load();
+            var delete = db.clients.Local.Where(p => p.Id == Convert.ToInt32(DeleteId.Text)).FirstOrDefault();
+            db.clients.Remove(delete);
+            db.SaveChanges();
+        }
+
+        private void updateC(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void createC(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            var db = new dbContext();
+            db.agents.Load();
+            var delete = db.agents.Local.Where(p => p.Id == Convert.ToInt32(DeleteId.Text)).FirstOrDefault();
+            db.agents.Remove(delete);
+            db.SaveChanges();
+        }
+
+        private void Create(object sender, RoutedEventArgs e)
+        {
+            var db = new dbContext();
+            var agent = new agents();
+            agent.FirstName = FN.Text;
+            agent.MiddleName = MN.Text;
+            agent.LastName = LN.Text;
+            agent.DealShare = Convert.ToInt32(DS.Text);
+            db.agents.Add(agent);
+            db.SaveChanges();
+
+
+        }
+
+
+
+        private void Update(object sender, RoutedEventArgs e)
+        {
+            var db = new dbContext();
+            db.agents.Load();
+            var agentUpdate = db.agents.Local.Where(p => p.Id == Convert.ToInt32(UpdateId.Text)).FirstOrDefault();
+           
+            agentUpdate.FirstName = FN1.Text;
+            agentUpdate.MiddleName = MN1.Text;
+            agentUpdate.LastName = LN2.Text;
+            agentUpdate.DealShare = Convert.ToInt32(DS1.Text); ;
+            db.SaveChanges();
+        }
+    }
+}
